fb830dd151171633d0b156e098dc2192fe89780d
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-07-11 21:23:57 -0300

    fix: correct some handles in the MCM blueprint

--- Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Client/_Init.lua
+++ Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Client/_Init.lua
@@ -1,22 +1,20 @@
 Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "General", function(tabHeader)
     local TPYButton = tabHeader:AddButton("Teleport party to you NOW")
-    local TPYDescription = tabHeader:AddText("Teleport party members to your location according to your settings.")
+    local TPYDescription = tabHeader:AddText("Teleport party members to the location of the last jumper according to your settings.")
     TPYButton.OnClick = function()
         Ext.Net.PostMessageToServer("FS_TeleportPartyToYou", Ext.Json.Stringify({ skipChecks = false }))
     end
 
     local TPYButtonForce = tabHeader:AddButton("Teleport party to you NOW (Force)")
     local TPYForceDescription = tabHeader:AddText(
-        "Teleport party members to your location regardless of your settings (combat, etc).")
+        "Teleport party members to the location of the last jumper regardless of your settings (combat, etc).")
     TPYButtonForce.OnClick = function()
         Ext.Net.PostMessageToServer("FS_TeleportPartyToYou", Ext.Json.Stringify({ skipChecks = true }))
     end
 end)
 
-local function handleKeyInput(e)
+Ext.Events.KeyInput:Subscribe(function(e)
     if KeybindingManager:IsKeybindingPressed(e, MCMGet("key_teleport_party_to_you")) then
         Ext.Net.PostMessageToServer("FS_TeleportPartyToYou", Ext.Json.Stringify({ skipChecks = false }))
     end
-end
-
-Ext.Events.KeyInput:Subscribe(handleKeyInput)
+end)
