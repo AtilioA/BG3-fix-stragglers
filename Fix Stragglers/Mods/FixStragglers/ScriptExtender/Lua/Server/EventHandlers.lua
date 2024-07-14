4512c2f46d35a905cf65711997e20df382245486
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-07-14 14:25:44 -0300

    refactor: use `VCHelpers.Net` methods

--- Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Server/EventHandlers.lua
+++ Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Server/EventHandlers.lua
@@ -1,23 +1,5 @@
 EHandlers = {}
 
--- Thanks to Aahz for this function
--- TODO: replace with VC method after it gets released
-function EHandlers.PeerToUserID(u)
-    -- all this for userid+1 usually smh
-    return (u & 0xffff0000) | 0x0001
-end
-
--- Returns the character that the user is controlling
-function EHandlers.GetUserCharacterUUID(userId)
-    for _, entity in pairs(Ext.Entity.GetAllEntitiesWithComponent("ClientControl")) do
-        if entity.UserReservedFor.UserID == userId then
-            return entity.Uuid.EntityUuid
-        end
-    end
-
-    return nil
-end
-
 function EHandlers.OnCastedSpell(params)
     if params.Spell == "Projectile_Jump" then
         JumpHandlerInstance:HandleJump(params)
@@ -43,8 +25,8 @@ function EHandlers.OnTPYButtonPress(call, payload, peerID)
 
     local skipChecks = parsedPayload.skipChecks
 
-    local userID = EHandlers.PeerToUserID(peerID)
-    local userCharacter = EHandlers.GetUserCharacterUUID(userID)
+    local userID = VCHelpers.Net:PeerToUserID(peerID)
+    local userCharacter = VCHelpers.Net:GetUserCharacterUUID(userID)
     if not userCharacter then
         FSWarn(0, "Could not find user character, aborting teleport.")
         return
