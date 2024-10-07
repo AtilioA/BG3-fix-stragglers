724bbef93b9985afda9d91fc95930b845151acdb
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-10-07 20:55:27 -0300

    refactor: hotreload localization if SE is Devel

--- Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Shared/_Init.lua
+++ Fix Stragglers/Mods/FixStragglers/ScriptExtender/Lua/Shared/_Init.lua
@@ -9,6 +9,33 @@ function RequireFiles(path, files)
     end
 end
 
+local function updateLoca()
+    for _, file in ipairs({ "FS_English.loca" }) do
+        local fileName = string.format("Localization/English/%s.xml", file)
+        local contents = Ext.IO.LoadFile(fileName, "data")
+
+        if not contents then
+            return
+        end
+
+        for line in string.gmatch(contents, "([^\r\n]+)\r*\n") do
+            local handle, value = string.match(line, '<content contentuid="(%w+)".->(.+)</content>')
+            if handle ~= nil and value ~= nil then
+                value = value:gsub("&[lg]t;", {
+                    ['&lt;'] = "<",
+                    ['&gt;'] = ">"
+                })
+                Ext.Loca.UpdateTranslatedString(handle, value)
+            end
+        end
+    end
+end
+
+if Ext.Debug.IsDeveloperMode() then
+    updateLoca()
+end
+
+
 -- function LoadStats(modDirectoryName, files)
 --     for _, file in ipairs(files) do
 --         local fileName = string.format("Public/%s/Stats/Generated/Data/%s.txt", modDirectoryName, file)
